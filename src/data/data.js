8aac0dfd4b4c7ff31c453346ef1a9c6e314bda4e
abdullahiabdi <cabudicali218@gmail.com>
2024-02-08 22:27:56 +0300

    Completed assignment

---
+++ src/data/data.js
@@ -0,0 +1,8 @@
+const todos = [
+    { id: 1, title: 'Read books'},
+    { id: 2, title: 'Eat pre workout'},
+    { id: 3, title: 'play football'},
+    { id: 4, title: 'Take a nap'},
+]
+
+export default todos;
